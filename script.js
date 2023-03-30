9da609587db2bc02133e9a9504b1ac4ab167653f
Scheunemannn <99615216+Scheunemannn@users.noreply.github.com>
2023-03-30 18:33:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const bordas = document.getElementById('main-container');
+
+const imagem = document.getElementById('imagem');
+
+let counter = 0
+
+
+
+const incremento = () => {
+    counter++
+    console.log(counter)
+    decremento()
+    const verificadora = () => {
+        if (counter == 1) {
+            imagem.style.opacity = '0'
+        }
+        else if (counter == 2) {
+            imagem.style.opacity = '1'
+            imagem.src = "assets/hinata.png"
+        }
+        else if (counter == 3) {
+            imagem.style.opacity = '0'
+        }
+        else if (counter == 4){
+            imagem.style.opacity = '1'
+            imagem.src = 'assets/zofia.png'
+        }
+        else if (counter == 5) {
+            imagem.style.opacity = '0'
+        }
+        else if (counter == 6) {
+            imagem.style.opacity = '1'
+            imagem.src = 'assets/fryda.png'
+        }
+        else if (counter == 7) {
+            imagem.style.opacity = '0'
+        }
+    }
+    verificadora()
+}
+
+const decremento = () => {
+    if (counter >= 8){
+    counter = 2}
+}
+
+
+
+setInterval(incremento, 1000)
